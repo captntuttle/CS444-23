7c3ad66cebd3493d26b79b482feeae59228a8ca9
Ken Steinfeldt <capt.tuttle@gmail.com>
2017-10-08 13:08:14 -0700

    create stack(ish) and start p and c funcs

--- concurrency/concurrency1/produce_consume.c
+++ concurrency/concurrency1/produce_consume.c
@@ -1,4 +1,4 @@
-/* Kristen Patterson & Kenneth Steinfelt
+/* Kristen Patterson & Kenneth Steinfeldt
  * Producer and Consumer Concurrency
  * Due: 10/09/17
  */
@@ -8,17 +8,54 @@
 #include <pthread.h>
 #include "mt19937ar.c"
 
+#define BUFFSIZE = 32
+
 struct buffer_entry{
 	int num; //random value, must not be 0 if "full"
 	int work_time; //random time to consume the thread
 };
 
 struct buffer_arr{
-	struct buffer_entry entries[32]; //size specified in assignment
+	struct buffer_entry entries[BUFFSIZE]; //size specified in assignment
+	int size; // current end of buffer struct array
 	pthread_mutex_t stop; //locks buffer
 };
 
 struct buffer_arr buffer;
+
+/* Name: push
+ * Description: Pushes a buffer_entry struct onto the buffer_arr stack
+ * Use: To add to buffer
+ * 
+ *
+ */
+void push(buffer_arr *b, buffer_entry e)
+{
+	if (b->iter < BUFFSIZE){ // make sure that buffer won't overflow
+		b->entries[b->size] = e; // place element in next open array spot
+		b->size++; // iterate forward by 1
+	}
+	else {
+		// shit's broke yo
+	}
+}
+
+/* Name: Pop
+ * Description: Returns top of stack
+ * Use: Get top of stack
+ *
+ *
+ */
+buffer_entry pop(buffer_arr *b)
+{
+	buffer_entry temp;
+	if (b->size > 0){
+		return b->entries[b->size--];
+	}
+	else {
+		// shit's empty yo
+	}
+}
 
 /* Name: check_buffer
  * Description: Checks buffer entries, returns 1 if empty, 2 if full, 0 if neither
@@ -42,6 +79,16 @@ int check_buffer()
 		temp = 0;
 
 	return temp;
+}
+
+void * producer()
+{
+
+}
+
+void * consumer()
+{
+
 }
 
 /* Name: random_num
