961dc15a632d8b8fe86297d5fcc487cdf102f10a
Kristen Patterson <patterkr@oregonstate.edu>
2017-10-24 23:10:08 -0700

    Added philosophers class

--- concurrency/concurrency2/philosophers.java
+++ concurrency/concurrency2/philosophers.java
@@ -23,14 +23,6 @@ class Chopstick{
 		this.id = id;
 	}
 
-	public void trylock(){
-		try{
-			sem.acquire();
-		}catch (InterruptedException ex){
-			Thread.currentThread().interrupt();
-		}
-	}
-
 	public synchronized void grabChopstick(String user){
 		while(this.mainuser != user){
 			try{
@@ -73,5 +65,65 @@ class Chopstick{
 
 	public void rel(){
 		sem.release();
+	}
+}
+
+class Philosopher extends Thread{
+	private String philo_name;
+	public Chopstick rightChopstick;
+	public Chopstick leftChopstick;
+
+	public Philosopher(String philo_name, Chopstick rightChopstick, Chopstick leftChopstick){
+		this.philo_name = philo_name;
+		this.rightChopstick = rightChopstick;
+		this.leftChopstick = leftChopstick;
+	}
+
+	public void think(){
+		int rand;
+		try{
+			System.out.println(philo_name + ": thinking...");
+
+			rand = (ThreadLocalRandom.current().nextInt(1, 20 + 1));
+			rand = rand * 1000;
+			Thread.sleep(rand);
+
+			System.out.println(philo_name + ": finished thinking");
+
+		}catch(InterruptedException ex){
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	public void eat(){
+		int rand;
+		try{
+			rightChopstick.acq();
+			leftChopstick.acq();
+			
+			System.out.println(philo_name + ": eating...");
+
+			rand = (ThreadLocalRandom.current().nextInt(2, 9 + 1));
+			rand = rand * 1000;
+			Thread.sleep(rand);
+
+			rightChopstick.rel();
+			leftChopstick.rel();
+
+			System.out.println(philo_name + ": finished eating");
+		}catch(InterruptedException ex){
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	public void actions(){
+		while(true){
+			think();
+			rightChopstick.grabChopstick(philo_name);
+			leftChopstick.grabChopstick(philo_name);
+			eat;
+			rightChopstick.setChopstick();
+			leftChopstick.setChopstick();
+		}
 	}
 }
